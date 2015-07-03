5498868234c13a407d4c17197896c4ac0897e1fe
v0lta <moritzwolter@gmx.de>
2015-07-03 13:08:10 +0200

    first stripes many numerical problems.

--- patterns/cellPatterns/modelComp.m
+++ patterns/cellPatterns/modelComp.m
@@ -14,9 +14,9 @@ Kn = 10^9;
 Kh = 4*10^8;
 
 % sumlation parameters
-dt = 0.1;
+dt = 0.01;
 dx = 0.1;
-tEnd = 1;
+tEnd = 0.1;
 gridLength = 1;
 
 %% test the muFunction.
@@ -30,30 +30,30 @@ plot(h,sols);
 
 
 %% numerical simulation.
-Rnew = zeros(floor(gridLength/dx),floor(gridLength/dx));
+%Initial cell density.
+Rnew = ones(floor(gridLength/dx),floor(gridLength/dx));
 R = Rnew;
+%AHL-concentrations
 Hnew = zeros(floor(gridLength/dx),floor(gridLength/dx));
 H = Hnew;
-Nnew = zeros(floor(gridLength/dx),floor(gridLength/dx));
+%Nutrient-levels.
+Nnew = ones(floor(gridLength/dx),floor(gridLength/dx));
 N = Nnew;
 
 for t = 1:1:floor(tEnd/dt)
     
-    muR = muFunction(H,DRhoMin,DRhoMax) * muR;
+    muR = muFunction(H,DRhoMin,DRhoMax) * R;
           
-    for i = 1:1:floor(gridLength/dx)
-        for j = 1:1:floor(gridLength/dx)
-            
-            
+    for i = 2:1:(floor(gridLength/dx)-1)
+        for j = 2:1:(floor(gridLength/dx)-1)
+                
             deltaR = (muR(i+1,j) + muR(i-1,j) + muR(i,j+1) + muR(i,j-1) - 4*muR(i,j))/dx^2; 
             deltaH = (H(i+1,j) + H(i-1,j) + H(i,j+1) + H(i,j-1) - 4*H(i,j))/dx^2;
             deltaN = (N(i+1,j) + N(i-1,j) + N(i,j+1) + N(i,j-1) - 4*N(i,j))/dx^2;
                     
-            Rnew(i,j) = R(i,j) + dt*(delaR + gamma*N(i,j)^2*R(i,j)) / (N(i,j)^2 + Kn^2);
+            Rnew(i,j) = R(i,j) + dt*(deltaR + gamma*N(i,j)^2*R(i,j) / (N(i,j)^2 + Kn^2));
             Hnew(i,j) = H(i,j) + dt*(Dh*deltaH + alpha*R(i,j)  - beta*H(i,j));
-            Nnew(i,j) = N(i,j) + dt*(Dn*deltaN - (kn * gamma * N(i,j)^2 * R(i,j))/(N(i,j)^2 + Kn^2);
-                  
-            
+            Nnew(i,j) = N(i,j) + dt*(Dn*deltaN - (kn * gamma * N(i,j)^2 * R(i,j))/(N(i,j)^2 + Kn^2));           
         end
     end
     R = Rnew;
@@ -62,5 +62,7 @@ for t = 1:1:floor(tEnd/dt)
             
 end 
 
-
+figure(1); clf; surf(H); title('H')
+figure(2); clf; surf(R); title('R')
+figure(3); clf; surf(N); title('N')
 
--- patterns/cellPatterns/muFunction.m
+++ patterns/cellPatterns/muFunction.m
@@ -2,9 +2,9 @@ function [ mu ] = muFunction( h,DRhoMin,DRhoMax )
 % The mu-cell mobility function.
 
 Kh = 4*10^8;
-m = 20;
+%m = 20; <- leads to trouble....
+m = 5;
 
-mu = (DRhoMax + DRhoMin .* (h./Kh).^m)/(1 + (h./Kh).^m);
-
+mu = (DRhoMax + DRhoMin .* (h./Kh).^m)./(1 + (h./Kh).^m);
 end
 
