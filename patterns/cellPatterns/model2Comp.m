838a29dc6a4b5e9f557a93f75cd69fe4a7ee6b61
v0lta <moritzwolter@gmx.de>
2015-07-06 18:07:31 +0200

    central diffs unstable?.

--- patterns/cellPatterns/model2Comp.m
+++ patterns/cellPatterns/model2Comp.m
@@ -6,18 +6,24 @@
 alpha = 3.0;
 beta  = 2.0;
 gamma = 0.2;
+%gamma = 0;
 rho   = 0.03;
 Dn    = 0.1;
 Dc    = 0.3;
 % Bifurcataion parameter.
-s = 0.5;
+s = 1;
+%pattern propagation.
+r = 0.9;
+%r = 0;
 
 length = 3;
-tend = 0.1;
+tend = 20;
 
-dt = 0.00001;
-J = 100;
-gps = 2:(J-1);
+dt = 0.005;
+J = 300;
+idx = 2:(J-1);
+
+dx = length/J;
 
 steps = floor(tend/dt)
 
@@ -28,6 +34,8 @@ steps = floor(tend/dt)
 N = 0.*x + 0.*y;
 N((J/2-10):(J/2+10),(J/2-10):(J/2+10)) = 1;
 Nnew = N;
+
+
 %transmitters are zero initially.
 C = 0.*x+0.*y;
 Cnew = C;
@@ -37,33 +45,36 @@ iChem = C;
 for t = 1:steps
     
       %compute the inner chemotaxis term.
-      iChem(gps,gps) = N(gps,gps) ./ ((1 + beta.*N(gps,gps)).^2) ...
-              .* (C(gps+1,gps) + C(gps-1,gps) + C(gps,gps+1) + ...
-              C(gps-1,gps) - 4.*C(gps,gps)./(dx^2)); 
+      iChem(idx,idx) = N(idx,idx) ./ ((1 + beta.*C(idx,idx)).^2) ...
+              .* ((C(idx+1,idx) + C(idx-1,idx) + C(idx,idx+1) + ...
+              C(idx,idx-1))./(2*dx)); 
             
                   %diffusion term.
-      Nnew(gps,gps) = N(gps,gps) + dt.*( Dn./(dx^2).*(N(gps+1,gps) + N(gps-1,gps) + ...
-                  N(gps,gps+1) + N(gps,gps-1) - 4.*N(gps,gps))  ...
+      Nnew(idx,idx) = N(idx,idx) + dt.*( Dn./(dx^2).*(N(idx+1,idx) + N(idx-1,idx) + ...
+                  N(idx,idx+1) + N(idx,idx-1) - 4.*N(idx,idx))  ...
                   ... %chemotaxis term.
-                  - (alpha./(dx.^2)).*(iChem(gps+1,gps) + iChem(gps-1,gps) ...
-                  + iChem(gps,gps+1) + iChem(gps,gps-1) - 4.*iChem(gps,gps)) ...
-                  ... %logistic growth.
-                  + rho.*N(gps,gps).*( 1  - N(gps,gps)./s));
+                  - (alpha./(2*dx)).*(iChem(idx+1,idx) + iChem(idx-1,idx) ...
+                  + iChem(idx,idx+1) + iChem(idx,idx-1)) ...
+                  ... %logistic growth, proliferation term.
+                  + rho.*N(idx,idx).*( 1  - N(idx,idx)./s));
         
       %Only the growth model.
       %Nnew(i,j) = N(i,j) + dt*( rho*N(i,j)*( 1  - N(i,j)/s));
-                
-      Cnew(gps,gps) = C(gps,gps) + dt*( Dc/(dx^2) .* (C(gps+1,gps) + ...
-                  C(gps-1,gps) + C(gps,gps+1) + ...
-                  C(gps,gps-1) - 4.*C(gps,gps))  ...
-                  + s.*N(gps,gps)./(1 + gamma.*N(gps,gps))  ...
-                  - C(gps,gps).*N(gps,gps)./(1 + gamma.*C(gps,gps)) );
+  
+                    %diffusion term
+      Cnew(idx,idx) = C(idx,idx) + dt*( Dc/(dx^2) .* (C(idx+1,idx) + ...
+                  C(idx-1,idx) + C(idx,idx+1) + ...
+                  C(idx,idx-1) - 4.*C(idx,idx))  ...
+                  ... %production term
+                  + s.*N(idx,idx)./(1 + gamma.*N(idx,idx))  ...
+                  ... %consumption term
+                  - (C(idx,idx).*(N(idx,idx).^r))./(1 + gamma.*C(idx,idx)) );
     
    N = Nnew;
    C = Cnew;
 end
 figure(1)
-surf(N);shading('flat');
+surf(real(N));shading('flat');
 figure(2)
-surf(C);shading('flat');
+surf(real(C));shading('flat');
 
