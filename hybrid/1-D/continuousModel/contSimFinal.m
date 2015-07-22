0dc570dee5788bf8262d0938c3e2dd7fafb85663
v0lta <moritzwolter@gmx.de>
2015-07-22 17:53:04 +0200

    conversion to hours.

--- hybrid/1-D/continuousModel/contSimFinal.m
+++ hybrid/1-D/continuousModel/contSimFinal.m
@@ -2,13 +2,13 @@
 %Model.
 
 %% Load constants.
-Da = 5*10^(-6); %(cm)^2/s;
-Db = 5*10^(-6); %(cm)^2/s;
-Dr = 7.3*10^(-6); %(cm)^2/s;
-Dh = 4.9*10^(-6); %(cm)^2/s;
+Da = 5*10^(-6)*3600; %(cm)^2/h;
+Db = 5*10^(-6)*3600; %(cm)^2/h;
+Dr = 7.3*10^(-6)*3600; %(cm)^2/h;
+Dh = 4.9*10^(-6)*3600; %(cm)^2/h;
 
-dt = 0.03; %s
-tend = 500; %s
+dt = 0.03; %h
+tend = 48; %h
 Xlen = 8.0; %cm
 J = 30; %#
 
@@ -23,8 +23,9 @@ H = zeros(int32(steps),J);
 
 preDif = zeros(1,J);
 
-Kc1 = 0.001;
-Kc2 = 0.005;
+Kc1 = 0.1;
+Kc2 = 0.5;
+gamma = 10^(-4);
 As = 1.0 * 10^2; % (cl)^(-1);
 Bs = 1.0 * 10^2; % (cl)^(-1);
 kr = 10^(-2);
@@ -33,7 +34,7 @@ idx = 2:1:(J-1);
 
 %% Set intial condition.
 x = meshgrid(linspace(0,Xlen,J));
-x = -10^2*(x - x.^2).*exp(-1 .*((x - 4).^2));
+x = -10*(x - x.^2).*exp(-1 .*((x - 4).^2));
 %Cell density of celly type A.
 A(1,:) = x(1,:) + abs(0.01*randn(1,length(x(1,:))));
 %A(1,(J/2-2):(J/2+2)) = 2;
@@ -50,8 +51,8 @@ for t = 1:1:steps
     
     %% Compute the next time step of the A-Type cells:
     %A(t+1,:) = A(t,:);
-    A(t+1,idx) = A(t,idx) + (dt.* Da)/dx^2  .* ( A(t,idx-1) + A(t,idx+1) - 2.*A(t,idx));
-               %  +A(t,idx) .* (1 - A(t,idx)./As);
+    A(t+1,idx) = A(t,idx) + (dt.* Da)/dx^2  .* ( A(t,idx-1) + A(t,idx+1) - 2.*A(t,idx)) ...
+                 +gamma.* A(t,idx) .* (1 - A(t,idx)./As);
     
     %% Compute the next time step of the B-Type cells:
     
@@ -64,8 +65,8 @@ for t = 1:1:steps
     B(t+1,idx) = B(t,idx) +  ...
                  dt/dx^2 .* (Db .* ( B(t,idx-1) + B(t,idx+1) - 2.*B(t,idx)) ...
                      - (betaP .* (R(t,idx+1) - R(t,idx)) ...
-                     -  betaN .* (R(t,idx)- R(t,idx-1))));
-                 %    + B(t,idx) .* (1 - B(t,idx)./Bs);
+                     -  betaN .* (R(t,idx)- R(t,idx-1))))...
+                     + gamma .* B(t,idx) .* (1 - B(t,idx)./Bs);
     
     %% Compute the repellant:
     R(t+1,idx) = R(t,idx) + dt*( Dr .* (R(t,idx+1) + R(t,idx-1) -  ...
@@ -90,13 +91,14 @@ for t = 1:1:steps
     B(t+1,1)   = B(t,1) + ...
                      dt/dx^2 .* (Db .* ( B(t,end) + B(t,2) - 2.*B(t,1)) ...
                      - (betaP .* (R(t,2) - R(t,1)) ...
-                     -  betaBeam .* (R(t,1)- R(t,end))));
+                     -  betaBeam .* (R(t,1)- R(t,end)))) ...
+                    + gamma .* B(t,1) .* (1 - B(t,1)./Bs);
     betaN   = (Xn(end-1) + Xn(end))./2;
     B(t+1,end) = B(t,end) + ...
                      dt/dx^2 .* (Db .* ( B(t,end-1) + B(t,1) - 2.*B(t,end)) ...
                      - (betaBeam .* (R(t,1) - R(t,end)) ...
                      -  betaN .* (R(t,end)- R(t,end-1)))) ...
-                     + B(t,end) .* (1 - B(t,end)./Bs);
+                     + gamma .* B(t,end) .* (1 - B(t,end)./Bs);
                  
     %Repellant             
     R(t+1,1) = R(t,1) + dt*( Dr .* (R(t,2) + R(t,end)...
@@ -115,8 +117,8 @@ end
 
 figure(1);clf;surf(A);shading('flat');xlabel('x');ylabel('time');title('cell A');zlabel('cell density');
 figure(2);clf;surf(B);shading('flat');xlabel('x');ylabel('time');title('cell B');zlabel('cell density');
-figure(3);clf;surf(R);shading('flat');xlabel('x');ylabel('time');title('repellant');zlabel('cell density');
-figure(4);clf;surf(H);shading('flat');xlabel('x');ylabel('time');title('AHL');zlabel('cell density');
+figure(3);clf;surf(R);shading('flat');xlabel('x');ylabel('time');title('repellant');zlabel('concentration');
+figure(4);clf;surf(H);shading('flat');xlabel('x');ylabel('time');title('AHL');zlabel('concentration');
 %figure(5);clf;surf(R-H);shading('flat');xlabel('x');ylabel('time');title('repellant-AHL');
 
 %% make movie:
