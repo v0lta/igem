f128fdef8e4ec83873b1db97bc4ed364f1bdb968
ThomasPak <thomas.pak.wu@gmail.com>
2015-08-04 11:48:47 +0200

    copied code from realModel

---
+++ hybrid/1-D/realModel_II/leucine.m
@@ -0,0 +1,187 @@
+classdef leucine<handle
+	properties
+		domain;
+		concentration;
+		gradient;
+		leftBoundary;
+		rightBoundary;
+	end
+
+	methods
+		function obj=leucine(domain,concentration,boundaries)
+		obj.domain=domain;
+		obj.concentration=concentration;
+		obj.calculategradient();
+		obj.leftBoundary=boundaries(1);
+		obj.rightBoundary=boundaries(2);
+		end
+		
+		function domain=getdomain(obj)
+		domain=obj.domain;
+		end
+		function concentration=getconcentration(obj)
+		concentration=obj.concentration;
+		end
+
+		function calculategradient(obj)
+		n=length(obj.concentration);
+		obj.gradient=obj.concentration*0;
+		h=obj.domain(2)-obj.domain(1);
+
+		obj.gradient(1)=(obj.concentration(2)-obj.concentration(1))/h;
+		obj.gradient(n)=(obj.concentration(n)-obj.concentration(n-1))/h;
+
+		for i=2:n-1
+			obj.gradient(i)=(obj.concentration(i+1)-obj.concentration(i-1))/(2*h);
+		end
+		end
+
+		function interpolatedValue=interpol(obj,field,xCoordinate)
+		%interpolate field concentration at bacterium position
+
+		%xCoordinate
+		%xmin=min(obj.domain)
+		%xmax=max(obj.domain)
+
+		x=obj.domain;
+		dx=x(2)-x(1);
+		C=field;
+		%length(C)
+		%C(1001)
+
+		n=length(x);
+
+		%%binary search for adjacent grid points
+		%kMin=1;
+		%kMax=n;
+		%k=floor((kMin+kMax)/2);
+
+		%while ~(x(k)<=xCoordinate && xCoordinate<=x(k+1))
+		%	%disp('New iteration');
+		%	%disp('xCoordinate');
+		%	%xCoordinate
+		%	%disp('x(k)');
+		%	%x(k)
+		%	%disp('x(kMin)');
+		%	%x(kMin)
+		%	%disp('x(kMax)');
+		%	%x(kMax)
+		%	%%disp('x(1)');
+		%	%%x(1)
+		%	%pause(1);
+		%	%coordinate smaller than x(k)
+		%	if xCoordinate<x(k)
+		%		kMax=k;
+		%		k=floor((kMin+kMax)/2);
+		%	%coordinate larger than x(k)
+		%	else
+		%		kMin=k+1;
+		%		k=floor((kMin+kMax)/2);
+		%	end
+		%end
+
+		%Search adjacent grid points
+		xDiff=xCoordinate-x(1);
+		kDiff=xDiff/dx;
+		k=floor(kDiff)+1;
+
+		%xCoordinate found between x(k) and x(k+1)
+		CLeft=C(k);
+		CRight=C(k+1);
+		xLeft=x(k);
+		xRight=x(k+1);
+
+		%calculate interpolated value
+		interpolatedValue=CLeft+(CRight-CLeft)/(xRight-xLeft)*(xCoordinate-xLeft);
+		end
+
+		function interpolatedConcentration=interpolconc(obj,xCoordinate)
+		field=obj.concentration;
+		interpolatedConcentration=obj.interpol(field,xCoordinate);
+		end
+
+		function interpolatedGradient=interpolgrad(obj,xCoordinate)
+		field=obj.gradient;
+		interpolatedGradient=obj.interpol(field,xCoordinate);
+		end
+
+		function update(obj,bacteriaDensity,Dleucine,beta,k2,dt)
+		%Update concentration based on bacteria density, diffusion constant of leucine,
+		%production rate (beta) , degradation rate (k2) and timestep
+		n=length(obj.domain);
+		dx=obj.domain(2)-obj.domain(1);
+		r=dt/dx^2;
+
+		%bacteria density
+		%zero flux
+		%rho=bacteriaDensity(obj.domain(1:end));
+		%fixed concentration
+		%rho=bacteriaDensity(obj.domain(2:end-1));
+		%periodic
+		rho=bacteriaDensity(obj.domain(1:end));
+
+		%Add right tail to left side
+		rightx=obj.domain(end);
+		i=1;
+		foo=bacteriaDensity(rightx+i*dx);
+
+		while foo~=0
+			rho(i)=rho(i)+foo;
+			i=i+1;
+			foo=bacteriaDensity(rightx+i*dx);
+		end
+
+		%Add left tail to right side
+		leftx=obj.domain(1);
+		i=1;
+		foo=bacteriaDensity(leftx-i*dx);
+
+		while foo~=0
+			rho(n+1-i)=rho(n+1-i)+foo;
+			i=i+1;
+			foo=bacteriaDensity(leftx-i*dx);
+		end
+
+		%matrix
+		%Zero flux boundary condition
+		%A=diag(ones(n,1)*(1+2*Dleucine*r));
+		%A=A+diag([-2*Dleucine*r;ones(n-2,1)*(-Dleucine*r)],1);
+		%A=A+diag([ones(n-2,1)*(-Dleucine*r);-2*Dleucine*r],-1);
+		%b=obj.concentration;
+
+		%fixed concentration boundary condition
+		%A=diag(ones(n-2,1)*(1+2*Dleucine*r));
+		%A=A+diag(ones(n-3,1)*(-Dleucine*r),1);
+		%A=A+diag(ones(n-3,1)*(-Dleucine*r),-1);
+		%b=obj.concentration(2:end-1);
+		%b(1)=b(1)+Dleucine*r*obj.leftBoundary;
+		%b(end)=b(end)+Dleucine*r*obj.rightBoundary;
+
+		%periodic boundary condition
+		A=diag(ones(n,1)*(1+2*Dleucine*r+k2*dt))...
+		+diag(ones(n-1,1)*(-Dleucine*r),1)...
+		+diag(ones(n-1,1)*(-Dleucine*r),-1);
+		A(end,1)=-Dleucine*r;
+		A(1,end)=-Dleucine*r;
+		b=obj.concentration;
+
+		b=b+rho*beta*dt;
+
+		%Calculate new concentration field
+		%zero flux
+		obj.concentration=(A\b')';
+		%fixed concentration
+		%obj.concentration=[obj.leftBoundary,(A\b')',obj.rightBoundary];
+
+		%correct for negative concentrations
+		for i=1:n
+			if obj.concentration(i)<1e-5
+				obj.concentration(i)=1e-5;
+			end
+		end
+
+		%update gradient
+		obj.calculategradient();
+		end
+	end
+end
---
+++ hybrid/1-D/realModel_II/main.m
@@ -0,0 +1,175 @@
+%Model I
+close all;clear all;clc;
+filename='test.avi';
+framerate=10;
+
+N=100;			%time steps
+nBacteriaA=100;	%number of bacteria A
+nBacteriaB=100;	%number of bacteria B
+%k=20;			%plot kth iteration
+L=15;			%Length of domain
+dx=0.1;			%grid spacing
+domain=[0:dx:L];%domain
+%domain=[0:5];%domain
+
+%define constants
+alpha=1e-3;		%bacterial production rate of AHL
+beta=1e-3;		%bacterial production rate of leucine
+k1=5e-3;		%degradation rate of AHL
+k2=5e-3;		%degradation rate of leucine
+DAHL=1/300;	%Diffusion constant of AHL
+Dleucine=2/300;%Diffusion constant of leucine
+%muHighA=1/30;	%high diffusion constant of bacteria A
+%muLowA=1/3000;	%low diffusion constant of bacteria A
+muHighA=1e-5;	%high diffusion constant of bacteria A
+muLowA=1e-5;	%low diffusion constant of bacteria A
+muHighB=1/30;	%high diffusion constant of bacteria B
+muLowB=1/300;	%low diffusion constant of bacteria B
+muA=[muLowA muHighA];
+muB=[muLowB muHighB];
+kappaA=2;		%chemotactic sensitivity constant bacteria A
+kappaB=2;		%chemotactic sensitivity constant bacteria B
+VthA=1.2;		%threshold concentration of AHL for bacteria A
+VthB=1.0;		%threshold concentration of AHL for bacteria B
+
+%initialize bacteria A
+bacteriaA=[];
+
+for i=1:nBacteriaA
+	%normal distribution
+	x=normrnd(L/2,1);
+
+	%uniform random distribution
+	%x=rand*L-L/2;
+
+	%uniform distribution
+	%x=L/(nBacteriaA+1)*i;
+
+	%single peak
+	%x=5;
+	%x=10;
+
+	b=bacterium(x);
+	bacteriaA=[bacteriaA b];
+end
+
+bacteriaPopA=bacteriaPopulationA(bacteriaA,domain);
+%bacteriaPopA=bacteriaPopulationStill(bacteriaA,domain);
+
+%initialize bacteria B
+bacteriaB=[];
+
+for i=1:nBacteriaB
+	%normal distribution
+	x=normrnd(L/2,1);
+
+	%uniform random distribution
+	%x=rand*L-L/2;
+
+	%uniform distribution
+	%x=L/(nBacteriaB+1)*i;
+
+	%single peak
+	%x=5;
+	%x=10;
+
+	b=bacterium(x);
+	bacteriaB=[bacteriaB b];
+end
+
+bacteriaPopB=bacteriaPopulationB(bacteriaB,domain);
+
+%initialize AHL field
+%uniform
+n=length(domain);
+concentration=zeros(1,n)+1;
+%sinusoidal
+%lambda=2;
+%k=2*pi/lambda;
+%%concentration=cos(domain)+1;
+%concentration=sin(k*domain)+1; 
+
+%boundary conditions
+boundaries=[1 1];
+AHLField=AHL(domain,concentration,boundaries);
+
+%initialize leucine field
+n=length(domain);
+concentration=zeros(1,n)+1;
+%concentration=cos(domain)+1;
+%concentration=sin(domain)+1;
+
+%boundary conditions
+boundaries=[1 1];
+leucineField=leucine(domain,concentration,boundaries);
+
+%define kernel function and bandwidth
+addpath ..\..\..\kernel;
+%kernelfun=@uni;
+%kernelfun=@tri;
+%kernelfun=@(x) normpdf(x,0,1);
+kernelfun=@epanechnikov;
+bandwidth=0.25;
+
+%define timestep
+timestep=1;
+
+%define scaling for plotting concentrations
+scaling=50;
+
+%initialize model
+model=realModel(bacteriaPopA,bacteriaPopB,AHLField,leucineField,...
+		alpha,beta,k1,k2,DAHL,Dleucine,muA,muB,kappaA,kappaB,VthA,VthB,...
+		kernelfun,bandwidth,timestep,scaling);
+
+runSimulation;
+makeVideo;
+
+beep on;
+beep;
+beep off;
+
+%% Junk code
+
+%% run for N (extra) time steps
+%N=300;
+%for i=1:N
+%	model.update()
+%end
+%
+%% beep on;
+%% beep;
+%% beep off;
+%
+%%plot kth iteration
+%%fig=figure(1);
+%%model.plot(k,fig);
+%%% 
+%%vidObj=VideoWriter('simulation2.avi');
+%vidObj=VideoWriter(filename);
+%set(vidObj,'FrameRate',framerate);
+%open(vidObj);
+%
+%nFrames=model.getlength();
+%fig=figure('Position',[100 100 1000 1000]);
+%for i=1:nFrames
+%	model.plot(i,fig);
+%	ylim([-5 75]);
+%	writeVideo(vidObj,getframe());
+%	clf;
+%end
+%
+%close(fig);
+%vidObj.close();
+
+%Double peak
+%for i=1:nBacteria/2
+%	x=-5;
+%	b=bacterium(x);
+%	bacteria=[bacteria b];
+%end
+%for i=(nBacteria/2+1):nBacteria
+%	x=5;
+%	b=bacterium(x);
+%	bacteria=[bacteria b];
+%end
