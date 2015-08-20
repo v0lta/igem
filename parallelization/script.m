135e8d9b90f59108e17d8296169b0426fc90d37e
ThomasPak <thomas.pak.wu@gmail.com>
2015-08-20 10:25:06 +0200

    Tested efficiency of gpuArrays in matrix operations

--- parallelization/script.m
+++ parallelization/script.m
@@ -1,18 +1,61 @@
 close all;clear all;clc;
-n=16;
-alpha=20;
 
-a=0;
-array=(1:n)+alpha;
+k=6144;
+%A=distributed(rand(k));
+A=gpuArray.rand(k);
+%A=rand(k);
 
-%% parallel for loop to compute sum of squares of numbers 1 to 5
-parfor i=1:n
-%for i=1:n
-	a=a+fib(array(i));
-    %A(i)=fib(array(i));
-end
+%B=distributed(rand(k));
+B=gpuArray.rand(k);
+%B=rand(k);
 
-%a=sum(A)
-a
+%serial
+Fserial=A\B;
+%Fserial=A*B;
+%Fserial=gather(Fserial);
 
-beep on; beep; beep off;
+%parallel
+%[m,n]=size(B);
+%
+%Fparallel=[];
+%parfor i=1:n
+%	Fparallel=[Fparallel,A*B(:,i)];
+%end
+
+%if sum(sum(Fserial==Fparallel)) ~= m*n
+%	disp('Matrices are not equal');
+%else
+%	disp('Matrices are equal');
+%end
+
+beep on;
+beep;
+beep off;
+
+%mldivide
+%k=6000
+%parallel: 59.413s
+%serial: 41.405s
+%gpu: 29.968s
+
+%serial: 19.209s
+%gpu: 26.995s
+%A gpu, B serial: 27.084s
+
+%serial: 23.866s
+%gpu: 26.330s
+
+%k=6144
+%serial
+%gpu
+
+%k=1000
+%parallel: 5.211s
+%serial: 0.545s
+%gpu: 0.408s
+%A gpu, B serial: 0.267s
+
+%matrix multiplication
+%k=6000
+%serial: 13.555s
+%gpu: 5.331s
