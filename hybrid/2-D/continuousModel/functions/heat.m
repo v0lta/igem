a8528719f90a981df56cbf2ec07cb69d01386747
v0lta <moritzwolter@gmx.de>
2015-09-08 18:40:42 +0200

    cleanup.

--- hybrid/2-D/continuousModel/functions/heat.m
+++
@@ -1,7 +0,0 @@
-function res = heat(B,D,dt,dx,i,j,J)
-
- %set the boundaries:
- [ ip1,im1,jp1,jm1 ] = setBounds( i,j,J );
- res = (dt.* D)./dx^2  .* ( B(ip1,j) + B(im1,j) + B(i,jp1) ...
-                            + B(i,jm1) - 4*B(i,j));
-end
--- hybrid/2-D/continuousModel/functions/logisticGrowth.m
+++
@@ -1,3 +0,0 @@
-function res = logisticGrowth(gamma,As,A,i,j,dt)
- res = dt .* ( gamma.* A(i,j) .* (1 - A(i,j)./As));
-end 
